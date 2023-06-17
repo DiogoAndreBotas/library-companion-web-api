5a1ff07a5f2c49acf7aca8956cbc15aaee7e8e33
DiogoAndreBotas <dbotas@salsify.com>
2023-06-17 23:47:02 +0100

    Add barebones Spring project

---
+++ src/main/kotlin/com/diogoandrebotas/librarycompanionwebapi/LibraryCompanionWebApiApplication.kt
@@ -0,0 +1,11 @@
+package com.diogoandrebotas.librarycompanionwebapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibraryCompanionWebApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<LibraryCompanionWebApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/diogoandrebotas/librarycompanionwebapi/LibraryCompanionWebApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.diogoandrebotas.librarycompanionwebapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryCompanionWebApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
