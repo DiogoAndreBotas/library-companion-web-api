999bd6b7b77844d9ac92ecd25b3c7649edbf9772
Diogo Botas <gamebreak24@gmail.com>
2023-07-01 19:55:54 +0100

    Add Ktor HTTP Client config class

---
+++ src/main/kotlin/com/diogoandrebotas/librarycompanionwebapi/config/HttpClientConfig.kt
@@ -0,0 +1,27 @@
+package com.diogoandrebotas.librarycompanionwebapi.config
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class HttpClientConfig {
+
+    private final val httpClient = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
+    }
+
+    @Bean
+    fun getHttpClient() = httpClient
+
+}
