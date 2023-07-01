3d3a6963a1685a7fbbba18d9ffa789fe9aaa4a3c
Diogo Botas <gamebreak24@gmail.com>
2023-07-01 19:55:27 +0100

    Add Ktor dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val ktorVersion = "2.3.2"
+
 plugins {
 	id("org.springframework.boot") version "3.1.0"
 	id("io.spring.dependency-management") version "1.1.0"
 	kotlin("jvm") version "1.8.21"
 	kotlin("plugin.spring") version "1.8.21"
+	kotlin("plugin.serialization") version "1.7.10"
 }
 
 group = "com.diogoandrebotas"
@@ -24,6 +27,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.postgresql:postgresql")
+	implementation("io.ktor:ktor-client-core:$ktorVersion")
+	implementation("io.ktor:ktor-client-cio:$ktorVersion")
+	implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+	implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
